1cffc2d3dbbb080db7b113677f89acfae773e6fc
Vladimir <v0ffk4@outlook.com>
2017-07-10 14:36:43 +0300

    Add template copy

--- gulpfile.js
+++ gulpfile.js
@@ -15,48 +15,49 @@ var gulp = require('gulp'),
 	rename = require('gulp-rename'),
 
 	//define directories
-	out = 'OUT/',
-	devTpl = 'dev/tpl/',
-	devImg = 'dev/images/',
-	devCss = 'dev/css/',
-	devJs = 'dev/js/'
+	out = 'OUT/'
+//	devTpl = 'dev/',
+//	devImg = 'dev/images/',
+//	devCss = 'dev/css/',
+//	devJs = 'dev/js/'
 
 //copy php
 gulp.task('tplCp', function() {
-	gulp.src(devTpl + '*.{php,tpl,html}')
+	gulp.src('dev/*.{php,tpl,html}')
 		.pipe(gulp.dest(out))
 		.pipe(livereload());
 });
 
-//compile SASS synthax / minify
-gulp.task('cssPrep', function() {
-	gulp.src(devCss + 'style.css')
-		.pipe(postcss([
-			precss(),
-			autoprefixer(),
-			cssnano()
-	]))
-	.on('error', gutil.log)
-		.pipe(gulp.dest(out + '/c'))
-		.pipe(livereload());
-});
+//	//compile SASS synthax / minify
+//	gulp.task('cssPrep', function() {
+//		gulp.src(devCss + 'style.css')
+//			.pipe(postcss([
+//				precss(),
+//				autoprefixer(),
+//				cssnano()
+//		]))
+//		.on('error', gutil.log)
+//			.pipe(gulp.dest(out + '/c'))
+//			.pipe(livereload());
+//	});
+//
+//	//concatinate & minify & rename javascript
+//	gulp.task('jsConcat', function(){
+//		gulp.src(devJs + '*.js')
+//			.pipe(concat('script.js'))
+//			.pipe(uglify())
+//			.pipe(rename({suffix: '.min'}))
+//			.pipe(gulp.dest(out + '/j'))
+//			.pipe(livereload());
+//	});
+//
+//	//watch & process
+//	gulp.task('watch', function() {
+//		livereload.listen();
+//		gulp.watch(devTpl + '**/*.{php,tpl,html}', ['tplCp']);
+//		gulp.watch(devCss + '**/*.css', ['cssPrep']);
+//		gulp.watch(devJs + '*.js', ['jsConcat']);
+//	});
 
-//concatinate & minify & rename javascript
-gulp.task('jsConcat', function(){
-	gulp.src(devJs + '*.js')
-		.pipe(concat('script.js'))
-		.pipe(uglify())
-		.pipe(rename({suffix: '.min'}))
-		.pipe(gulp.dest(out + '/j'))
-		.pipe(livereload());
-});
-
-//watch & process
-gulp.task('watch', function() {
-	livereload.listen();
-	gulp.watch(devTpl + '**/*.{php,tpl,html}', ['tplCp']);
-	gulp.watch(devCss + '**/*.css', ['cssPrep']);
-	gulp.watch(devJs + '*.js', ['jsConcat']);
-});
-
-gulp.task( 'default', [ 'tplCp', 'cssPrep', 'jsConcat', 'watch' ] );
+gulp.task( 'default', [ 'tplCp' ] );
+//gulp.task( 'default', [ 'tplCp', 'cssPrep', 'jsConcat', 'watch' ] );
